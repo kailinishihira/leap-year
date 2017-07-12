bfd74045d289a883016de60c99320cfd5552e291
Kaili Nishihira and Michael Woldemedihin <kaili@>
2017-07-12 09:47:53 -0700

    Implement first behavior which checks if the input is divisible by 4.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,9 @@
 var leapYear = function(year){
+
+  debugger;
+  if (year % 4 === 0) {
+    return true;
+} else
   return false;
 }
 
@@ -8,10 +13,10 @@ $(document).ready(function(){
   $("#leap-year").submit(function(event){
     event.preventDefault();
     var userInput = parseInt($("#year").val());
-    var result = leapYear(year);
+    var result = leapYear(userInput);
     $("#result").text(result);
 
-    leapYear(userInput);
+    //leapYear(userInput);
 
   });
 });
