692b9191990f1e38a0d8ce58c8386d1bbe08478c
Kaili Nishihira and Michael Woldemedihin <michael@>
2017-07-12 10:05:10 -0700

    Implement all behaviors that are listed in README.md file.

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,19 @@
+// var leapYear = function(year){
+//  var result;
+//   debugger;
+//   if (year % 4 === 0 || year % 400 === 0) {
+//     result = true;
+// } else if (year % 100 === 0) {
+//   result = false;
+// } else{
+//   result = false;
+// }
 var leapYear = function(year){
-
-  debugger;
-  if (year % 4 === 0) {
+  if ((year % 4 === 0) && (year % 100 !== 0 || year % 400 === 0)) {
     return true;
-} else
-  return false;
+  }else{
+    return false;
+  }
 }
 
 
